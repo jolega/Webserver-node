3ac2c593a78158f1c83a675ddaa4847bd4b3e857
jolega <alonso_juancho@hotmail.com>
2022-06-08 07:58:30 -0500

    usando template

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ app.get('/hello-world',  (req, res) => {
     res.send('Hello World in route') ;
   })
 
+  app.get('/generic',  (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html') ;
+  })
+
+  app.get('/elements',  (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html') ;
+  })
 
   app.get('*',  (req, res) => {
     res.sendFile(__dirname + '/public/404.html') ;
